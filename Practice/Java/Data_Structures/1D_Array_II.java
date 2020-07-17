6bb8d9d58e42ee7dcf0fcfbe35696137cd091cc3
harishankarsivaji <harishs92@gmail.com>
2020-07-17 15:42:11 +0100

    Data structures array

---
+++ Practice/Java/Data_Structures/1D_Array_II.java
@@ -0,0 +1,1 @@
+1D_Array_II
---
+++ Practice/Java/Data_Structures/2D_Array.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        scanner.close();
+        System.out.println(maxHourGlass(arr));
+    }
+
+    public static int maxHourGlass(int[][] arr){
+        int max = Integer.MIN_VALUE;
+        for (int i= 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                int sum = findSum(arr, i, j);
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+    
+    private static int findSum(int [][] arr, int r, int c) {
+        int sum = arr[r+0][c+0] + arr[r+0][c+1] + arr[r+0][c+2] 
+                                + arr[r+1][c+1] + 
+                  arr[r+2][c+0] + arr[r+2][c+1] + arr[r+2][c+2];
+        return sum;
+    }
+}
